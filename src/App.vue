f91a452464941bd7d810bfbbebd35edb9a23e5d2
Israel Silva <israel@iasdigitalgroup.com>
2024-09-20 21:03:31 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <header>
+      <span>Vue.js PWA</span>
+    </header>
+    <main>
+      <img src="./assets/logo.png" alt="Vue.js PWA">
+      <hello></hello>
+    </main>
+  </div>
+</template>
+
+<script>
+import Panel from "./components/Panel.vue";
+import InputForm from "./components/InputForm";
+import Result from "./components/Result";
+import { calcTaxes } from "./calc.js";
+
+export default {
+  name: 'app',
+  components: {
+    Panel,
+    InputForm,
+    Result
+  }
+}
+</script>
+
+<style lang="scss" src="./assets/styles/App.scss"/>
